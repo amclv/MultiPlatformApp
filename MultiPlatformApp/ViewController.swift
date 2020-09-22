a3e5bb7661d8fa1f2973527e59f6daeb2fd2a3e1
Aaron <toscleveland@gmail.com>
2020-09-21 20:42:23 -0500

    “Init Proj”

---
+++ MultiPlatformApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MultiPlatformApp
+//
+//  Created by Aaron Cleveland on 9/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 150/255, green: 150/255, blue: 150/255, alpha: 1.0)
+    }
+}
+
